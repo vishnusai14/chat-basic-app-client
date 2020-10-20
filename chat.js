bceadcbd6d2cd9f76f292f462198e592155f60dd
vishnusai14 <67537099+vishnusai14@users.noreply.github.com>
2020-10-20 14:16:28 +0530

    Update chat.js

--- chat.js
+++ chat.js
@@ -5,7 +5,7 @@ app.use(express.static(__dirname + "/public"));
 app.get("/", function(req, res) {
     res.redirect("/chat.html")
 })
-const server = app.listen(8000, function() { console.log("Server Strated") });
+const server = app.listen(process.env.PORT, function() { console.log("Server Strated") });
 const io = socketio(server)
 io.on('connect', (socket) => {
     socket.on("messageToServer", (msg) => {
