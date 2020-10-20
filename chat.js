89272ab42eeae0c8186111e31e2ba5f8709e549b
vishnusai14 <67537099+vishnusai14@users.noreply.github.com>
2020-10-20 17:53:56 +0530

    Update chat.js

--- chat.js
+++ chat.js
@@ -2,6 +2,9 @@ const express = require("express")
 const socketio = require("socket.io");
 const app = express();
 app.use(express.static(__dirname + "/public"));
+app.get("/" , function(req,res){
+    res.redirect("/chat.html")
+})
 const server = app.listen(process.env.PORT || 3000);
 const io = socketio(server);
 //We are Connecting to the client and receiving the msg form "msgText" event and emitting not to the one socket but to all th io
