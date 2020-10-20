c1b702deeb4a33f1bcc84bf847b8784d3e1332f6
vishnusai14 <67537099+vishnusai14@users.noreply.github.com>
2020-10-20 17:48:43 +0530

    Update chat.js

--- chat.js
+++ chat.js
@@ -1,18 +1,12 @@
-const express = require("express");
+const express = require("express")
 const socketio = require("socket.io");
 const app = express();
 app.use(express.static(__dirname + "/public"));
-app.get("/", function(req, res) {
-    res.redirect("/chat.html")
-})
-const server = app.listen(process.env.PORT, function() { console.log("Server Strated") });
-const io = socketio(server)
-io.on('connect', (socket) => {
-    socket.on("messageToServer", (msg) => {
-        // console.log(msg)
+const server = app.listen(process.env.PORT || 3000);
+const io = socketio(server);
+//We are Connecting to the client and receiving the msg form "msgText" event and emitting not to the one socket but to all th io
+io.on("connection", (socket) => {
+    socket.on("msgText", function(msg) {
+        io.emit("msg", msg)
     })
-    socket.on('chatMsg', (chat) => {
-        io.emit('chatMsgTo', chat)
-    })
-    socket.emit('messageFromServer', "Hi Client This is a Message From Server")
 })
